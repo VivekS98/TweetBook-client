1194ad5b000cff21cf648c614d11fc5a1d9e955b
Vivek Kashyap <vivekyeskashyap@gmail.com>
2022-09-13 16:09:53 +0530

    Added cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("https://tweetbook-in.herokuapp.com/");
+  });
+});
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,49 +1,55 @@
-import React from 'react';
-import '../styling/main.css';
-import { connect } from 'react-redux';
+import React from "react";
+import "../styling/main.css";
+import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 const Welcome = (props) => {
-    const history = useHistory();
-    if(props.auth) {
-        history.push('/home');
-        return <p>loading...</p>;
-    } else {
-        return (
-            <React.Fragment>
-                <header className="header">
-                    <h2 className="logo">TweetBook</h2>
-                    <div className="nav">
-                        <Button 
-                          component="div"
-                          color="inherit"
-                          className="nav-button"
-                          onClick={() => history.push('/signup')}>
-                              Signup
-                        </Button>
-                        <Button 
-                          component="div"
-                          color="inherit"
-                          className="nav-button"
-                          onClick={() => history.push('/signin')}>
-                              Login
-                        </Button>
-                    </div>
-                </header>
-                <div className="background-img">
-                    <div className="welcome">
-                        <h1 className="welcome-text">Welcome to TweetBook!</h1>
-                        <p className="welcome-desc"><b>Login/Signup</b> to explore more...</p>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
-}
+  const history = useHistory();
+  if (props.auth) {
+    history.push("/home");
+    return <p>loading...</p>;
+  } else {
+    return (
+      <React.Fragment>
+        <header className="header">
+          <h2 className="logo">TweetBook</h2>
+          <div className="nav">
+            <Button
+              component="div"
+              color="inherit"
+              className="nav-button"
+              data-testId="signup"
+              onClick={() => history.push("/signup")}
+            >
+              Signup
+            </Button>
+            <Button
+              component="div"
+              color="inherit"
+              className="nav-button"
+              data-testId="login"
+              onClick={() => history.push("/signin")}
+            >
+              Login
+            </Button>
+          </div>
+        </header>
+        <div className="background-img">
+          <div className="welcome">
+            <h1 className="welcome-text">Welcome to TweetBook!</h1>
+            <p className="welcome-desc">
+              <b>Login/Signup</b> to explore more...
+            </p>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+};
 
-const mapStateToProps = state => ({
-    auth: state.currentUser.isAuthenticated
+const mapStateToProps = (state) => ({
+  auth: state.currentUser.isAuthenticated,
 });
 
 export default connect(mapStateToProps, null)(Welcome);
